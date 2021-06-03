aad182fd9721014c7d87db761976b3040fdf8319
Dmitriy Zelenskiy <d.zelenskiy@gmail.com>
2021-06-03 14:15:41 -0500

    Created Spring Boot project

---
+++ src/main/java/com/github/dzelenskiy/myretailrestfulservice/MyretailRestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.dzelenskiy.myretailrestfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyretailRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyretailRestfulServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dzelenskiy/myretailrestfulservice/MyretailRestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dzelenskiy.myretailrestfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyretailRestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
