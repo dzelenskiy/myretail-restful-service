151d15f1cc6302afeda911ed4078cd1f782fb2c4
Dmitriy Zelenskiy <d.zelenskiy@gmail.com>
2021-06-05 20:59:03 -0500

    Added ProductPriceFacadeImpl.getProductWithCurrentPriceById logic

--- src/main/java/com/github/dzelenskiy/myretailrestfulservice/facades/ProductPriceFacadeImpl.java
+++ src/main/java/com/github/dzelenskiy/myretailrestfulservice/facades/ProductPriceFacadeImpl.java
@@ -1,13 +1,33 @@
 package com.github.dzelenskiy.myretailrestfulservice.facades;
 
+import com.github.dzelenskiy.myretailrestfulservice.dtos.CurrentPrice;
 import com.github.dzelenskiy.myretailrestfulservice.dtos.Product;
+import com.github.dzelenskiy.myretailrestfulservice.dtos.ProductDetails;
+import com.github.dzelenskiy.myretailrestfulservice.services.CurrentPriceService;
+import com.github.dzelenskiy.myretailrestfulservice.services.ProductDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductPriceFacadeImpl implements ProductPriceFacade {
 
+    @Autowired
+    private ProductDetailsService productDetailsService;
+
+    @Autowired
+    private CurrentPriceService currentPriceService;
+
     @Override
     public Product getProductWithCurrentPriceById(int id) {
-        return new Product();
+
+        ProductDetails productDetails = productDetailsService.getProductDetailsById(id);
+        CurrentPrice currentPrice = currentPriceService.getCurrentPriceByProductId(id);
+
+        Product product = new Product();
+        product.setId(id);
+        product.setName(productDetails.getItem().getProductDescription().getTitle());
+        product.setCurrentPrice(currentPrice);
+
+        return product;
     }
 }
